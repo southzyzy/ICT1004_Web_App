2cfa0ae8b86f6525ae7823aaef98fa12f45f5b54
Zhao Xiang Lim <developer@zxlim.xyz>
2019-03-04 10:52:17 +0800

    Merged About and Features

---
+++ docs/static/js/nav.js
@@ -0,0 +1,11 @@
+/*
+	For partial fulfillment of the module requirements for ICT1004 Web Systems and Technologies.
+	Infocomm Technology Cluster, Singapore Institute of Technology.
+*/
+
+$(document).ready(function() {
+	$(".no-href").on("click", function(e) {
+		e.preventDefault();
+		return false;
+	});
+});
