6e762d7ddc00d9c526c016952e3add40e96a2fe5
Zhao Xiang Lim <developer@zxlim.xyz>
2019-03-07 23:16:28 +0800

    Scroll to top

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ICT1004 Web Systems and Technologies
-Redesign for http://www.meiheongyuendessert.com.sg
+
+## Project Summary
+This is a redesign of the [Mei Heong Yuen Dessert Website](http://www.meiheongyuendessert.com.sg).
 
 
-See: https://southzyzy.github.io/ICT1004_Web_App
+## Hosted on
+GitHub:  https://southzyzy.github.io/ICT1004_Web_App
+ICT1004: http://ict1004.ddns.net/dco/group08/project1
--- docs/static/js/nav.js
+++ docs/static/js/nav.js
@@ -3,9 +3,35 @@
 	Infocomm Technology Cluster, Singapore Institute of Technology.
 */
 
+
+function display_top() {
+	if ($(window).scrollTop() > 100) {
+		$("#to-top").fadeIn();
+	}
+	else {
+		$("#to-top").fadeOut();
+	}
+}
+
 $(document).ready(function() {
 	$(".no-href").on("click", function(e) {
 		e.preventDefault();
 		return false;
 	});
+	
+
+	$("#to-top").on("click touchstart", function(e){
+		e.preventDefault();
+		
+		$("html, body").stop().animate({
+			scrollTop: 0,
+		}, "slow", "swing");
+
+		return false;
+	});
+
+	display_top();
+
+	$(document).on("scroll", display_top);
+
 });
